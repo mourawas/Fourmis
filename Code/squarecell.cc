391d1e683778391466877a6e11b5a209dac278a0
Baixas Louis Giovanni Franco <baixas@INTRANET.EPFL.CH@INJ-IN-SC-36.intranet.epfl.ch>
2022-03-11 18:05:07 +0100

    grille

--- Code/squarecell.cc
+++ Code/squarecell.cc
@@ -1,4 +1,14 @@
 #include <iostream>
+#include <vector>
 #include "error_squarcell.h"
 using namespace std;
 
+class Grille {
+	private:
+	vector<vector<bool>> grid;
+	const int g_max
+	public:
+	Grille(int g_max_);
+	:g_max (g_max_) {}
+	
+	
