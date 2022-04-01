86d4e841715641706047afe70ae4a7e0e35b696f
spyrboy <louis.carterre@gmail.com>
2022-04-01 13:31:12 +0200

    fourmis
    edfc

--- Code/fourmis.h
+++ Code/fourmis.h
@@ -18,12 +18,40 @@ public:
     unsigned int getage(){
         return age;
     }
-    
-
-
 };
+
 class Generator : public Fourmis {
     private :
+    unsigned int side = sizeG;
     public :
-}
+    Generator (unsigned int x1, unsigned int y1, unsigned int age,)
+    : x1(x1),y1(y1),age(age)
+    {}
+};
+
+class Collector : public Fourmis{
+private:
+    bool food;
+public:
+    Collector ( unsigned int x1, unsigned y1, unsigned int age ,bool food)
+    : x1(x1),y1(y1),age(age),food(food)
+    {}
+};
+
+class Defensor : public Fourmis {
+private:
+public:
+    Defensor (unsigned int x1, unsigned int y1, unsigned age)
+    :x1(x1),y1(y1),age(age)
+    {}
+};
+
+class Predator : public Fourmis {
+private:
+public:
+    Predator ( unsigned int x1,unsigned y1, unsigned age)
+    :x1(x1),y1(y1),age(age)
+    {}
+};
+
 vector < Fourmis > vectFourmis;
