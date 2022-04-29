d2535c3e9eb1950a065affda304368d09503bc19
spyrboy <louis.carterre@gmail.com>
2022-04-29 12:21:46 +0200

    gh
    gvhj

--- Code/squarecell.h
+++ Code/squarecell.h
@@ -48,11 +48,13 @@ void initialise_Carre_Centre(Carre& c);
 
 bool Carre_dans_Carre(Carre& c1, Carre& c2);
 
-void dessine_carre_croix(double x,double y,double size, double r,double g, double b);
+void dessin_carre_croix(double x,double y,double size, double r,double g, double b);
 
-void dessine_carre_plus(double x, double y, double size, double r, double g, double b);
+void dessin_carre_plus(double x, double y, double size, double r, double g, double b);
 
-void dessine_carre_plein(double x,double y,double size, double r, double g, double b);
+void dessin_carre_plein(double x,double y,double size, double r, double g, double b);
 
-void dessine_losange(double x, double y, double r, double b, double g);
+void dessin_losange(double x, double y, double r, double b, double g);
+
+void dessin_carre_vide( double x, double y, double size, double r, double g, double b);
 #endif
