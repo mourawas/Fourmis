c55636dbc89cb122f5300e63018d7d6ba06c1166
Luzgord <77929733+Luzgord@users.noreply.github.com>
2022-05-21 13:22:30 +0200

    Delete Code directory

--- Code/graphic_gui.h
+++
@@ -1,9 +0,0 @@
-#ifndef GRAPHIC_GUI_H_INCLUDED
-#define GRAPHIC_GUI_H_INCLUDED
-
-#include <gtkmm.h>
-#include "graphic.h"
-
-void graphic_set_context(const Cairo::RefPtr<Cairo::Context>& cr);
-
-#endif
